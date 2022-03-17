f28485f91d051d82215eb72496c190766c7a511d
Charles Chen <yifuchen@Charless-MacBook-Pro.local>
2022-03-17 11:43:29 -0700

    added metadata and imputer

---
+++ src/preprocess/Imputer.py
@@ -0,0 +1,25 @@
+imputation_dict = {
+#     "bi_rads_score": "",
+#     "tumor_stge": "",
+    "abnormal_lymph": 2,
+    "lymph_node_max_size_mm": 0,
+    "extent_of_calcification_ma": 0,
+    "prominent_axillary_lymph": 2,
+    "backgroun_enhancement": 2,
+    "max_enhancement_measurement": 0,
+    "axillary_lymphadenopathy": 2,
+    "internal_mammary_lymphaden": 2,
+#     "high_grade_fdg_foci_presen": "",
+#     "size_of_the_largest_foci_c": "",
+    "axillary_lymphadenopathy_p": 0,
+#     "axillary_lymph_node_max_si":"",
+    "internal_mammry_lymph_nod": 0,
+    "er_status": 0.5,
+    "pr_status": 0.5,
+    "her_status": 0.5,
+    "axillary_lymph_node_core_b": 0,
+}
+
+def __init__(self):
+    pass
+
